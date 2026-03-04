f21e27269e992039b892d474f2994251c00d9167
Michael Dorokhov <dorohov1008@gmail.com>
2026-03-04 15:28:44 +0200

    Initial commit Task 2

---
+++ pyCore_mod5_task2.py
@@ -0,0 +1,20 @@
+import re
+from typing import Callable
+
+def generator_numbers(text: str):   
+    pattern = r"\b\d+\.\d{2}\b"
+    for match in re.finditer(pattern, text):
+        yield float(match.group())
+
+
+
+def sum_profit(text: str, func: Callable): 
+     sum = float(0.0)
+     for number in func(text):
+         sum += number
+     return sum
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
